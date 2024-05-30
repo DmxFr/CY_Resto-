a1559dd0badc64a82cb829feaaa3ebb9888bac51
DmxFr <130449732+DmxFr@users.noreply.github.com>
2024-05-30 20:21:57 +0200

    Update annuler_commande.php

--- annuler_commande.php
+++ annuler_commande.php
@@ -1,11 +1,9 @@
 <?php
-// Démarrer la session
+
 session_start();
 
-// Réinitialiser le panier
 unset($_SESSION['panier']);
 
-// Rediriger vers la page d'accueil
 header("Location: index.php");
 exit();
 ?>
