d1a606d96679df279c1f44189b593300d278f34c
DmxFr <130449732+DmxFr@users.noreply.github.com>
2024-05-30 20:53:53 +0200

    Update traitement_panier.php

--- traitement_panier.php
+++ traitement_panier.php
@@ -7,13 +7,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['action'] == 'confirmer') {
         $idUtilisateurs = isset($_SESSION['idUtilisateurs']) ? $_SESSION['idUtilisateurs'] : null;
         if ($idUtilisateurs === null) {
-            // Insérer un utilisateur temporaire
             $query = "INSERT INTO utilisateurs_temp (idTemp) VALUES (NULL)";
             $conn->query($query);
             $idUtilisateurs = $conn->insert_id;
             $_SESSION['idUtilisateurs'] = $idUtilisateurs;
         }
-        // Insérer la commande
         foreach ($_SESSION['panier'] as $type => $items) {
             foreach ($items as $item) {
                 $query = "INSERT INTO commandes (idUtilisateurs, plat, quantité, adresse, ville, code_postal, telephone, dateCommande) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
@@ -23,11 +21,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $stmt->close();
             }
         }
-        $_SESSION['panier'] = []; // Vider le panier après confirmation
+        $_SESSION['panier'] = []; 
         header("Location: confirmation_commande.php");
         exit();
     } elseif ($_POST['action'] == 'annuler') {
-        $_SESSION['panier'] = []; // Vider le panier
+        $_SESSION['panier'] = []; 
         header("Location: index.php");
         exit();
     }
