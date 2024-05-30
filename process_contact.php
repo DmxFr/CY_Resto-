b2f83968107212acbf07897810329c3c2216ba0e
DmxFr <130449732+DmxFr@users.noreply.github.com>
2024-05-30 20:42:04 +0200

    Update process_contact.php

--- process_contact.php
+++ process_contact.php
@@ -16,8 +16,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'deosamuntu@gmail.com'; // Remplacez par votre adresse Gmail
-        $mail->Password = 'hyud skth lpmb ydhl'; // Remplacez par votre mot de passe d'application
+        $mail->Username = 'deosamuntu@gmail.com'; 
+        $mail->Password = 'hyud skth lpmb ydhl';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
