b6b4c698e3fafca374f955949f18654ec48a7493
DmxFr <130449732+DmxFr@users.noreply.github.com>
2024-05-30 20:43:34 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,21 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-// Récupération des données du formulaire
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Vous pouvez ajouter ici des validations supplémentaires et sauvegarder les données dans une base de données
-
-// Redirection vers la page de commande
+  
 header("Location: commande.php");
 exit();
 } else {
-// Si la requête n'est pas POST, rediriger vers le formulaire
 header("Location: formulaire.html");
 exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -26,6 +23,6 @@ exit();
 <body>
 <h2>Page de Commande</h2>
 <p>Bienvenue, vous pouvez maintenant passer votre commande.</p>
-<!-- Ajoutez ici le formulaire de commande ou la liste des menus -->
+  
 </body>
 </html>
